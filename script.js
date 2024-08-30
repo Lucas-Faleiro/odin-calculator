bdda5d866133a158218e47f0615678bfdd1c5884
lucas-faleiro <16035833+Lucas-Faleiro@users.noreply.github.com>
2024-08-30 12:24:43 -0300

    Add operate function

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+let firstNum = 0;
+let secondNum = 0;
+let operator = "";
+
 const add = (numA, numB) => numA + numB;
-
 const sub = (numA, numB) => numA - numB;
-
 const mult = (numA, numB) => numA * numB;
-
 const divide = (numA, numB) => numA / numB;
+
+const operate = (operation, numA, numB) => {
+  switch (operation) {
+    case "+":
+      return add(numA, numB);
+    case "-":
+      return sub(numA, numB);
+    case "*":
+      return mult(numA, numB);
+    case "/":
+      return divide(numA, numB);
+  }
+};
